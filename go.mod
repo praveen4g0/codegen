d64ef277e372e5c046640ead641b268494134321
thangadanchp <thangadanchp@vmware.com>
2024-12-02 23:17:40 +0530

    Feat: go:generate to generate json struct
    Signed-off-by: thangadanchp <thangadanchp@vmware.com>

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/praveen4g0/codegen
+
+go 1.22.6
+
+require golang.org/x/text v0.20.0
---
+++ pkg/generate.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"reflect"
+	"strings"
+	"text/template"
+
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+)
+
+//go:generate go run generate.go
+//go:generate go fmt codegen/user.gen.go
+
+func main() {
+	var resp map[string]interface{}
+	in, _ := os.Open("../user.json")
+	b, _ := io.ReadAll(in)
+	json.Unmarshal(b, &resp)
+
+	caser := cases.Title(language.English)
+
+	data := struct {
+		Name   string
+		Fields map[string]interface{}
+	}{
+		"User",
+		resp,
+	}
+
+	tpl, err := template.New("template.tpl").Funcs(template.FuncMap{
+		"Title": caser.String,
+		"TypeOf": func(v interface{}) string {
+			if v == nil {
+				return "string"
+			}
+			return strings.ToLower(reflect.TypeOf(v).String())
+		},
+	}).ParseFiles("template.tpl")
+
+	if err != nil {
+		fmt.Errorf("failed to parse template: %v", err)
+	}
+
+	out, _ := os.Create("codegen/user.gen.go")
+	defer out.Close()
+
+	if err = tpl.Execute(out, data); err != nil {
+		fmt.Errorf("failed to execute template: %v", err)
+	}
+}
